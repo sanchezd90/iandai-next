6231ad45c2ac832ae20e39e0933e347c53a8c871
Daniel <daniel.rueda@ibisdev.tech>
2024-02-09 22:22:39 -0300

    auth logic

--- src/app/components/loginButton.tsx
+++ src/app/components/loginButton.tsx
@@ -1,12 +1,23 @@
 'use client'
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function LoginButton() {
+  const { data: session } = useSession()
+  console.log(session);
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <button onClick={() => signIn("google")}>
-      Login with Google
-    </button>
-  );
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 }
 
 export default LoginButton;
--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -2,7 +2,8 @@
 
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return <SessionProvider><Provider store={store}>{children}</Provider></SessionProvider>;
 }
