4c890fa60a3e5d0783f59fad21c9e5f94d5c74bf
Daniel <daniel.rueda@ibisdev.tech>
2024-02-21 22:22:38 -0300

    redirect to /en

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
     env: {
         API_BASE_URL: process.env.API_BASE_URL,
     },
+    redirects: async () => {
+        return [
+          {
+            source: '/',
+            destination: '/en',
+            permanent: true, // Use 308 for permanent redirect
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig
--- src/app/[locale]/StartButton.tsx
+++ src/app/[locale]/StartButton.tsx
@@ -8,6 +8,6 @@ export const StartButton = () => {
     const intl = useIntl();
     const router = useRouter()
   return (
-    <Button variant='outlined' onClick={()=>router.push('/desk')} style={{width:'80%',maxWidth:'350px'}}> {intl.formatMessage({ id: 'lets_start' })}</Button>
+    <Button variant='outlined' onClick={()=>router.push(`${intl.locale}/desk`)} style={{width:'80%',maxWidth:'350px'}}> {intl.formatMessage({ id: 'lets_start' })}</Button>
   )
 }
